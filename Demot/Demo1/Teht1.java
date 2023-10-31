e724da5090d249ec0eb3bcefcd369574b97b246e
atteket <atte@kettunen.com>
2023-10-31 16:03:46 +0200

    Demo1Teht1

--- Demot/Demo1/Teht1.java
+++ Demot/Demo1/Teht1.java
@@ -2,8 +2,34 @@ package Algoritmit.Demot.Demo1;
 
 public class Teht1 {
 
-    public boolean onkoJaollinen() {
-        
+    public static void main(String args[]) {
+        System.out.println(onkoJaollinen(5, 2));
+        System.out.println(onkoJaollinen(6, 2));
+        System.out.println(onkoJaollinen(100, 2));
+        System.out.println(alkuMerkitPerakkain("testi"));
+        System.out.println(alkuMerkitPerakkain(""));
+        System.out.println(alkuMerkitPerakkain("aaron"));
+
     }
 
+    public static boolean onkoJaollinen(int jaettava, int jakaja) {
+        if (jakaja == 0) return false;
+        if (jaettava == 0) return true;
+        int jakojaannos = jaettava;
+        while (jakojaannos >= jakaja) {
+            jakojaannos -= jakaja;
+        }
+        return jakojaannos == 0;
+    }
+
+    public static int alkuMerkitPerakkain(String s) {
+        if (s == null|| s.length() == 0) return 0;
+        int count = 0;
+        char eka = s.charAt(0);
+        for (char c : s.toCharArray()) {
+            if (c == eka) count++;
+            else break;
+        }
+        return count;
+    }
 }
